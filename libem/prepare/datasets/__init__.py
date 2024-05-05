2aea681cc4a0c06575e24f654d4e4cf5e196c954
zenodflow <zenodflow@gmail.com>
2024-05-05 00:15:03 -0700

    feat: add datasets loader
    Signed-off-by: zenodflow <zenodflow@gmail.com>

---
+++ libem/prepare/datasets/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+LIBEM_SAMPLE_DATA_PATH = os.path.join(os.path.dirname(__file__),
+                                      '..', '..', '..', '..',
+                                      'libem-sample-data')
