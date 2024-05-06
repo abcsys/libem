b34db59357103ca63c194f748bc0ceceeabedf04
daiwaid <66648633+daiwaid@users.noreply.github.com>
2024-05-05 21:30:32 -0700

    feat: add dataset loaders and benchmarks (#1)

---
+++ libem/prepare/datasets/dblp_acm.py
@@ -0,0 +1,65 @@
+import os
+import json
+
+import libem.prepare.datasets as datasets
+
+path = os.path.join(datasets.LIBEM_SAMPLE_DATA_PATH, "dblp-acm")
+test_file = os.path.join(path, "test.ndjson")
+train_file = os.path.join(path, "train.ndjson")
+valid_file = os.path.join(path, "valid.ndjson")
+
+
+def read_test(schema=True):
+    # sample data:
+    # {"id_left":"dblp_1835","title_left":"wavelet-based histograms for selectivity estimation","authors_left":"jeffrey scott vitter , yossi matias , min wang","venue_left":"sigmod conference","year_left":1998,"cluster_id_left":2110,
+    # "id_right":"acm_184","title_right":"wavelet-based histograms for selectivity estimation","authors_right":"yossi matias , jeffrey scott vitter , min wang","venue_right":"international conference on management of data","year_right":1998,"cluster_id_right":2110,
+    # "label":1,"pair_id":"dblp_1835#acm_184"}
+    with open(test_file) as f:
+        for line in f:
+            data = json.loads(line.strip())
+            parsed_data = {'left': {}, 'right': {}, 'label': data.get('label', None)}
+
+            # clean the data
+            trim = ["cluster_id_left", "cluster_id_right", "id_left", "id_right"]
+            if schema:
+                for key, value in data.items():
+                    if key in trim:
+                        continue
+                    if key.endswith('_left'):
+                        new_key = key[:-5]  # Remove '_left'
+                        parsed_data['left'][new_key] = value
+                    elif key.endswith('_right'):
+                        new_key = key[:-6]  # Remove '_right'
+                        parsed_data['right'][new_key] = value
+            else:
+                left_values, right_values = [], []
+                for key, value in data.items():
+                    if key in trim:
+                        continue
+                    # Skip null values
+                    if value is None:
+                        continue
+                    if key.endswith('_left'):
+                        left_values.append(str(value))
+                    elif key.endswith('_right'):
+                        right_values.append(str(value))
+                parsed_data['left'] = ' '.join(left_values)
+                parsed_data['right'] = ' '.join(right_values)
+
+            yield parsed_data
+
+
+def read_train():
+    raise NotImplementedError
+
+
+def read_valid():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    import pprint
+
+    pp = pprint.PrettyPrinter(sort_dicts=False)
+    pp.pprint(next(read_test()))
+    pp.pprint(next(read_test(schema=False)))
---
+++ libem/prepare/datasets/walmart_amazon.py
@@ -0,0 +1,65 @@
+import os
+import json
+
+import libem.prepare.datasets as datasets
+
+path = os.path.join(datasets.LIBEM_SAMPLE_DATA_PATH, "walmart-amazon")
+test_file = os.path.join(path, "test.ndjson")
+train_file = os.path.join(path, "train.ndjson")
+valid_file = os.path.join(path, "valid.ndjson")
+
+
+def read_test(schema=True):
+    # sample data:
+    # {"id_left":"walmart_88","title_left":"balt wheasel easel adjustable melamine dry erase board white","category_left":"stationery & office machinery","brand_left":"balt","modelno_left":"33250","price_left":239.88,"cluster_id_left":463,
+    # "id_right":"amazon_3269","title_right":"balt inc. wheasel easel adjustable melamine dry erase board 28 3 4 x 59 1 2 white","category_right":"laminating supplies","brand_right":"mayline","modelno_right":null,"price_right":134.45,"cluster_id_right":463,
+    # "label":1,"pair_id":"walmart_88#amazon_3269"}
+    with open(test_file) as f:
+        for line in f:
+            data = json.loads(line.strip())
+            parsed_data = {'left': {}, 'right': {}, 'label': data.get('label', None)}
+
+            # clean the data
+            trim = ["cluster_id_left", "cluster_id_right", "id_left", "id_right"]
+            if schema:
+                for key, value in data.items():
+                    if key in trim:
+                        continue
+                    if key.endswith('_left'):
+                        new_key = key[:-5]  # Remove '_left'
+                        parsed_data['left'][new_key] = value
+                    elif key.endswith('_right'):
+                        new_key = key[:-6]  # Remove '_right'
+                        parsed_data['right'][new_key] = value
+            else:
+                left_values, right_values = [], []
+                for key, value in data.items():
+                    if key in trim:
+                        continue
+                    # Skip null values
+                    if value is None:
+                        continue
+                    if key.endswith('_left'):
+                        left_values.append(str(value))
+                    elif key.endswith('_right'):
+                        right_values.append(str(value))
+                parsed_data['left'] = ' '.join(left_values)
+                parsed_data['right'] = ' '.join(right_values)
+
+            yield parsed_data
+
+
+def read_train():
+    raise NotImplementedError
+
+
+def read_valid():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    import pprint
+
+    pp = pprint.PrettyPrinter(sort_dicts=False)
+    pp.pprint(next(read_test()))
+    pp.pprint(next(read_test(schema=False)))
