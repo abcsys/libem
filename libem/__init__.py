356daa1e0f7ec646d89d3599204b01a261f9f9f8
zenodflow <zenodflow@gmail.com>
2024-05-08 14:47:16 -0700

    feat: add libem logging
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- libem/__init__.py
+++ libem/__init__.py
@@ -1,4 +1,4 @@
-import libem.function
+from libem import function
 
 name = "libem"
 func = function.func
@@ -6,3 +6,10 @@ schema = function.schema
 
 from libem.constant import *
 from libem.interface import *
+from libem.core.util import *
+
+from libem.core.log import (
+    info, warn, error, debug,
+    enable_log, disable_log
+)
+from libem.core.trace import trace
--- libem/constant.py
+++ libem/constant.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import logging
 
 LIBEM_DIR = os.path.join(os.path.expanduser("~"), ".libem")
 LIBEM_CONFIG_FILE = os.path.join(LIBEM_DIR, "config.yaml")
@@ -12,3 +13,10 @@ try:
 except FileNotFoundError:
     print(f"Config file not found at {LIBEM_CONFIG_FILE}")
     sys.exit(1)
+
+LIBEM_DO_LOG = False
+LIBEM_LOG_DIR = os.path.join(
+    os.path.dirname(__file__),
+    "..", "logs")
+LIBEM_LOG_LEVEL = logging.INFO
+LIBEM_3RD_PARTY_LOG_LEVEL = logging.WARNING
