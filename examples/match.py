7ed243f804fac4b91d4b0fac4cf379a98438922d
zenodflow <zenodflow@gmail.com>
2024-05-09 15:21:49 -0700

    improve examples
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- examples/match.py
+++ examples/match.py
@@ -2,8 +2,8 @@ import libem
 
 
 def positive():
-    e1 = "Supplier X, Item: Widget, Qty: 100, Price: $5"
-    e2 = "X Supplier, Widget, Quantity: 100, Unit Price: $5"
+    e1 = "Dyson Hot+Cool AM09 Jet Focus heater and fan, White/Silver"
+    e2 = "Dyson AM09 Hot + Cool Jet Focus Fan Heater - W/S"
 
     is_match = libem.match(e1, e2)
 
