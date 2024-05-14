45406bb2fef0e533bdf2b12603af74360905ac16
David <davidspacewang@gmail.com>
2024-05-14 08:51:30 -0700

    fix: slight formatting fix in prompt.py

--- libem/match/prompt.py
+++ libem/match/prompt.py
@@ -1,7 +1,7 @@
 from libem.core.struct import Prompt
 
 query = Prompt(
-    default="Do the two entity descriptions refer to the same real-world entity? "
+    default="Do the two entity descriptions refer to the same real-world entity?\n"
             "Entity 1: {left}.\nEntity 2: {right}.",
     options=[""],
 )
@@ -15,7 +15,7 @@ experience = Prompt(
 )
 
 output = Prompt(
-    default="Answer with 'Yes' if they do and 'No' if they do not.\n",
+    default="Answer with 'Yes' if they do and 'No' if they do not.",
     options=["Explain your answer step by step and end with 'yes' or 'no' only."  # CoT
              "Answer only 1 or 0.",
              "Answer with 'yes' or 'no' only, in lower case.", ],
