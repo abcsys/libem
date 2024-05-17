09556decaf3b9d06da792e4a52b8bf787e335ae5
zenodflow <zenodflow@gmail.com>
2024-05-16 23:17:45 -0700

    feat: improve trace and add trace stats
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- examples/trace.py
+++ examples/trace.py
@@ -6,9 +6,12 @@ pp = pprint.PrettyPrinter(sort_dicts=False)
 
 def main():
     e1 = "Dyson Hot+Cool AM09 Jet Focus heater and fan, White/Silver"
-    e2 = "Dyson AM09 Hot + Cool Jet Focus Fan Heater - Black japan"
+    e2 = "Dyson AM09 Hot + Cool Jet Focus Fan Heater - W/S japan"
 
     print("Trace 1:")
+    libem.calibrate({
+        "libem.match.parameter.tools": [],
+    })
     with libem.trace as t:
         _ = libem.match(e1, e2)
         pp.pprint(t.get())
@@ -17,9 +20,13 @@ def main():
     e2 = "ZGMF/A-262PD-P"
 
     print("Trace 2:")
+    libem.calibrate({
+        "libem.match.parameter.tools": ["libem.browse"],
+    })
     with libem.trace as t:
         _ = libem.match(e1, e2)
         pp.pprint(t.get())
+        pp.pprint(t.stats())
 
 
 if __name__ == '__main__':
