b4e248916639eb00f5807d892ad5c6191684276a
David <davidspacewang@gmail.com>
2024-05-28 16:19:29 -0700

    feat: added more telemetry reporting in results file

--- benchmark/function.py
+++ benchmark/function.py
@@ -14,7 +14,7 @@ from libem.prepare.datasets import (
     abt_buy, amazon_google, dblp_acm,
     walmart_amazon, dblp_scholar
 )
-from libem.core.eval import precision, recall, f1
+from libem.core.eval import confusion_matrix, precision, recall, f1
 
 random.seed(libem.LIBEM_SEED)
 datasets = {
@@ -27,6 +27,8 @@ datasets = {
 
 
 def benchmark(args):
+    total_start_time = time.time()
+    
     if not args.verbose:
         libem.LIBEM_LOG_LEVEL = logging.WARNING
     
@@ -42,8 +44,8 @@ def benchmark(args):
     if args.shuffle:
         random.shuffle(dataset)
     
-    start_time = time.time()
-    tokens_used = 0
+    # telemetry
+    total_input_tokens, total_output_tokens = 0, 0
     
     print(f"Benchmark: Matching {args.num_pair if args.num_pair > 0 else 'all'}"
           f" {'pair' if args.num_pair == 1 else 'pairs'}"
@@ -62,26 +64,38 @@ def benchmark(args):
             print(f"Entity 1: {e1}\nEntity 2: {e2}")
 
         # call match
-        is_match = None
         with libem.trace as t:
+            is_match = None
+            start_time = time.time()
+            
             while is_match is None:
-                try: # retry if model times out
+                # retry if model times out
+                num_timeouts = 0
+                try:
                     is_match = libem.match(e1, e2)
                 except libem.ModelTimedoutException:
-                    pass
+                    num_timeouts += 1
+                if args.verbose and num_timeouts > 0:
+                    print(f"Model timed out {num_timeouts} time(s).")
             
-            # get unparsed model output
+            # get unparsed model output and telemetry
+            latency = time.time() - start_time
             pred = [i['match']['model_output'] for i in t.get() if 'match' in i][0]
-            tokens_used += sum([i['model']['tokens'] for i in t.get() if 'model' in i])
+            input_tokens = sum([i['model']['num_input_tokens'] for i in t.get() if 'model' in i])
+            output_tokens = sum([i['model']['num_output_tokens'] for i in t.get() if 'model' in i])
+            total_input_tokens += input_tokens
+            total_output_tokens += output_tokens
             
-            # cache results
+            # append results
             result.append({
                 'entity_1': e1,
                 'entity_2': e2,
-                'tools_used': [i['tool'] for i in t.get() if 'tool' in i],
-                'model_output': pred,
                 'pred': is_match,
-                'label': label
+                'label': label,
+                'model_output': pred,
+                'tools_used': [i['tool'] for i in t.get() if 'tool' in i],
+                'latency': round(latency, 2),
+                'tokens': {'input_tokens': input_tokens, 'output_tokens': output_tokens}
             })
 
         # track results for evaluation metrics
@@ -109,9 +123,20 @@ def benchmark(args):
     
     # get stats
     metrics = [precision, recall, f1]
+    conf_mat = confusion_matrix(np.array(truth), np.array(predictions))
     stats = {m.__name__: round(m(np.array(truth), np.array(predictions)) * 100, 2) for m in metrics}
-    stats['latency'] = round(time.time() - start_time, 2)
-    stats['tokens'] = tokens_used
+    stats['latency'] = round(time.time() - total_start_time, 2)
+    stats['tokens'] = {
+        'input_tokens': total_input_tokens,
+        'output_tokens': total_output_tokens
+    }
+    stats['confusion_matrix'] = {
+        'tp': int(conf_mat[0]),
+        'tn': int(conf_mat[2]),
+        'fp': int(conf_mat[1]),
+        'fn': int(conf_mat[3])
+    }
+    
 
     with open(out_file, 'w') as f:
         json.dump({
