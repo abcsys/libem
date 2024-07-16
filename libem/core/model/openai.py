7221ac7cf39248baa9c0e347c335850086220d27
zenodflow <zenodflow@gmail.com>
2024-07-16 15:25:54 -0700

    refactor: make model a core module
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- libem/core/model/openai.py
+++ libem/core/model/openai.py
@@ -1,8 +1,6 @@
 import os
 import json
-import time
 import httpx
-import platform
 import importlib
 
 from openai import (
@@ -12,38 +10,22 @@ from openai import (
 import libem
 from libem.core.util import run_async_task
 
-
-def call(*args, **kwargs) -> dict:
-    return run_async_task(
-        async_call(*args, **kwargs)
-    )
-
-
-async def async_call(*args, **kwargs) -> dict:
-    if kwargs.get("model", "") == "llama3":
-        return llama(*args, **kwargs)
-    else:
-        return await async_openai(*args, **kwargs)
-
-
-""" OpenAI """
-
 os.environ.setdefault(
     "OPENAI_API_KEY",
     libem.LIBEM_CONFIG.get("OPENAI_API_KEY", "")
 )
 
-_openai_client = None
+_client = None
 
 
-def get_openai_client():
-    global _openai_client
+def get_client():
+    global _client
 
     if not os.environ.get("OPENAI_API_KEY"):
         raise EnvironmentError(f"OPENAI_API_KEY is not set.")
 
-    if not _openai_client:
-        _openai_client = AsyncOpenAI(
+    if not _client:
+        _client = AsyncOpenAI(
             http_client=httpx.AsyncClient(
                 limits=httpx.Limits(
                     max_connections=1000,
@@ -51,17 +33,17 @@ def get_openai_client():
                 )
             )
         )
-    return _openai_client
+    return _client
 
 
-def openai(*args, **kwargs) -> dict:
+def call(*args, **kwargs) -> dict:
     return run_async_task(
-        async_openai(*args, **kwargs)
+        async_call(*args, **kwargs)
     )
 
 
 # Model call with multiple rounds of tool use
-async def async_openai(
+async def async_call(
         prompt: str | list | dict,
         tools: list[str] = None,
         context: list = None,
@@ -70,7 +52,7 @@ async def async_openai(
         seed: int = None,
         max_model_call: int = 3,
 ) -> dict:
-    client = get_openai_client()
+    client = get_client()
 
     context = context or []
 
@@ -200,11 +182,13 @@ async def async_openai(
                 tool_calls = response_message.tool_calls
 
                 num_model_calls += 1
-                num_input_tokens += response.usage.total_tokens - response.usage.completion_tokens
+                num_input_tokens += response.usage.total_tokens - \
+                                    response.usage.completion_tokens
                 num_output_tokens += response.usage.completion_tokens
 
             if num_model_calls == max_model_call:
-                libem.debug(f"[model] max call reached: {messages}\n{response_message}")
+                libem.debug(f"[model] max call reached: "
+                            f"{messages}\n{response_message}")
 
     """End call"""
 
@@ -232,96 +216,6 @@ async def async_openai(
     }
 
 
-_llama_model, _llama_tokenizer = None, None
-
-""" Llama """
-
-
-def llama(prompt: str | list | dict,
-          tools: list[str] = None,
-          context: list = None,
-          model: str = "llama3",
-          temperature: float = 0.0,
-          seed: int = None,
-          max_model_call: int = 3,
-          ) -> dict:
-    global _llama_model, _llama_tokenizer
-
-    # format the prompt to messages
-    match prompt:
-        case list():
-            messages = prompt
-        case dict():
-            messages = []
-            for role, content in prompt.items():
-                if content:
-                    messages.append({"role": role, "content": content})
-        case str():
-            messages = [{"role": "user", "content": prompt}]
-        case _:
-            raise ValueError(f"Invalid prompt type: {type(prompt)}")
-
-    BOS = "<|begin_of_text|>"
-    SYS = "<|start_header_id|>system<|end_header_id|>"
-    USER = "<|start_header_id|>user<|end_header_id|>"
-    ASSIS = "<|start_header_id|>assistant<|end_header_id|>"
-    EOS = "<|eot_id|>"
-
-    input_text = BOS
-    for message in messages:
-        if message['role'] == 'system':
-            input_text = input_text + SYS + message['content'] + EOS
-        elif message['role'] == 'user':
-            input_text = input_text + USER + message['content'] + EOS
-    input_text = input_text + ASSIS
-
-    context = context or []
-    messages = context + [input_text]
-
-    # apple silicon
-    if platform.machine() == "arm64" and platform.system() == "Darwin":
-        # Load the model using MLX for apple silicon device
-        if model == "llama3":
-            # first check whether mlx_lm is installed
-            try:
-                from mlx_lm import load, generate
-            except ImportError:
-                raise ImportError("mlx_lm is not installed.")
-            model_path = "mlx-community/Meta-Llama-3-8B-Instruct-4bit"
-        else:
-            raise ValueError(f"{model} is not supported.")
-
-        if _llama_model is None or _llama_tokenizer is None:
-            start = time.time()
-            _llama_model, _llama_tokenizer = load(model_path)
-            libem.debug(f"model loaded in {time.time() - start:.2f} seconds.")
-        else:
-            libem.debug("model loaded from cache")
-        model, tokenizer = _llama_model, _llama_tokenizer
-
-        if tools:
-            raise libem.ToolUseUnsupported("Tool use is not supported")
-
-        response = generate(model, tokenizer, prompt=messages[0], temp=temperature)
-    else:
-        raise ValueError(f"{platform.machine()} {platform.system()} is not supported.")
-
-    return {
-        "output": response,
-        "messages": "messages is not supported",
-        "tool_outputs": "Tool output is not supported",
-        "stats": {
-            "num_model_calls": 1,
-            "num_input_tokens": -1,
-            "num_output_tokens": -1,
-        }
-    }
-
-
 def reset():
-    global _openai_client
-    global _llama_model
-    global _llama_tokenizer
-    _openai_client = None
-    _llama_model = None
-    _llama_tokenizer = None
+    global _client
+    _client = None
