f1261e8620cfb49164bd9824aea185f378026aeb
Silvery D. Fu <163332118+zenodflow@users.noreply.github.com>
2024-07-16 15:29:12 -0700

    refactor: make model a core module (#41)
    * refactor: make model a core module
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>
    
    * fix reset methods
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>
    
    ---------
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>

---
+++ libem/core/model/__init__.py
@@ -0,0 +1,22 @@
+from libem.core.model import (
+    openai, llama
+)
+from libem.core.util import run_async_task
+
+
+def call(*args, **kwargs) -> dict:
+    return run_async_task(
+        async_call(*args, **kwargs)
+    )
+
+
+async def async_call(*args, **kwargs) -> dict:
+    if kwargs.get("model", "") == "llama3":
+        return llama.call(*args, **kwargs)
+    else:
+        return await openai.async_call(*args, **kwargs)
+
+
+def reset():
+    openai.reset()
+    llama.reset()
