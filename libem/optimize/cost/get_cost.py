e9ee9781f1d5094de47f2e7681bec5765939d2f3
Char15Xu <charlesxuty3@berkeley.edu>
2024-07-21 13:05:20 -0700

    updating with rebased main

--- libem/optimize/cost/get_cost.py
+++
@@ -1,8 +0,0 @@
-from libem.optimize.cost.openai import get_cost as get_cost_openai
-
-def get_cost(model, num_input_tokens, num_output_tokens):
-    if model == "llama3":
-        return 0
-    else:
-        return get_cost_openai(model, num_input_tokens, num_output_tokens)
-
