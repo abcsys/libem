1c72f007621d2a8be75328c77e7c6d5665c2fafc
zenodflow <zenodflow@gmail.com>
2024-07-27 23:19:02 -0700

    feat: improve likelihood examples
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- examples/tool/likelihood.py
+++ examples/tool/likelihood.py
@@ -15,5 +15,18 @@ def main():
     print("Match likelihood:", is_match["answer"])
 
 
+    left, right = "apple inc.", "Applebee's"
+    is_match = libem.match(left, right)
+
+    print("Entity 1:", left, "\nEntity 2:", right)
+    print("Match likelihood:", is_match["answer"])
+
+    left, right = "apple", "Apple electronics"
+    is_match = libem.match(left, right)
+
+    print("Entity 1:", left, "\nEntity 2:", right)
+    print("Match likelihood:", is_match["answer"])
+
+
 if __name__ == '__main__':
     main()
