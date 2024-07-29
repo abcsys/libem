8968fb2c34de90f19e656f70c5a23d2c3d80c0b5
zenodflow <zenodflow@gmail.com>
2024-07-29 15:38:39 -0700

    fix: add default right value for block
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- libem/block/interface.py
+++ libem/block/interface.py
@@ -5,6 +5,6 @@ from libem.block import func
 
 def block(
         left: Iterator[str | dict],
-        right: Iterator[str | dict] | None
+        right: Iterator[str | dict] | None = None
 ) -> Iterator[dict]:
     yield from func(left, right)
