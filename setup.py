ae329aad47605cd9d6a639a3da69742764113c5f
zenodflow <zenodflow@gmail.com>
2024-08-02 14:44:25 -0700

    Bump version to v0.0.23
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="libem",
-    version="0.0.22",
+    version="0.0.23",
     description="Libem python library",
-    author="System Design Studio",
+    author="Systems Design Studio",
     author_email="team@SD.studio",
     license="Apache License, Version 2.0",
     packages=find_packages(exclude=("tests",)),
