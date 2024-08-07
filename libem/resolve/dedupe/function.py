a324470c9d1865423fb14a908617a47c664c1188
zenodflow <zenodflow@gmail.com>
2024-08-06 23:47:17 -0700

    feat(dedupe): ensure schema consistency of in/output
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- libem/resolve/dedupe/function.py
+++ libem/resolve/dedupe/function.py
@@ -12,8 +12,18 @@ def func(records):
                 libem.info("[dedupe] cluster_id not found in records, "
                            "do clustering first")
                 records: pd.DataFrame = libem.cluster(records)
-            return records.drop_duplicates("cluster_id", keep="first")
+                return records.drop_duplicates("cluster_id", keep="first") \
+                    .drop("cluster_id", axis=1).reset_index(drop=True)
+
+            return records.drop_duplicates("cluster_id", keep="first") \
+                .reset_index(drop=True)
         case list():
-            return list({record["cluster_id"]: record for record in records}.values())
+            if "cluster_id" not in records[0]:
+                libem.info("[dedupe] cluster_id not found in records, "
+                           "do clustering first")
+                records = libem.cluster(records)
+                return list({record[0]: record for record in records}.values())
+
+            return list({record[0]: record for record in records}.values())
         case _:
             raise NotImplementedError(f"Unsupported type: {type(records)}")
