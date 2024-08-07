004a7bbddbea50182b59b14042c9e4600d94ba5d
Silvery D. Fu <163332118+zenodflow@users.noreply.github.com>
2024-08-06 19:47:56 -0700

    feat: configurable deep resolution of callable parameter (#77)
    * feat: deep resolve callable parameter
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>
    
    * feat: allow configuring recurse in parameter call
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>
    
    ---------
    
    Signed-off-by: zenodflow <zenodflow@gmail.com>

--- libem/core/struct/parameter.py
+++ libem/core/struct/parameter.py
@@ -37,12 +37,14 @@ class Parameter(Tunable):
             self.options = {i: o for i, o in enumerate(self.options)}
         super().__init__()
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, recurse=True, **kwargs):
         """
         When called, the instance evaluates and returns its current value. If the
-        value is a string, it formats the string with supplied args and kwargs.
+        value is a string, it formats the string with supplied args and kwargs. If
+        the value is a callable, it calls the function with supplied args and kwargs.
         """
         value = self.value
+
         if isinstance(value, Index):
             try:
                 value = self.options[self.value.key]
@@ -51,13 +53,21 @@ class Parameter(Tunable):
                                f"not found in options "
                                f"{list(self.options.keys())}.")
 
-        if isinstance(value, str):
+        if callable(value) and recurse:
+            return value(*args, **kwargs)
+        elif isinstance(value, str):
             return value.format(*args, **kwargs)
         else:
             return value
 
     def __str__(self):
         return str(self.__call__())
+
+    def __eq__(self, other):
+        if isinstance(other, Parameter):
+            return self.value == other.value
+        else:
+            return self.value == other
 
     def __repr__(self):
         return self.__str__()
