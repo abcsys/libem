8df2d1db9cbcea5321a1302c325fb5254d566599
Char15Xu <charlesxuty3@berkeley.edu>
2024-08-06 17:21:31 -0700

    move libem.match.prompt.output calibration to model/init.py

--- libem/core/model/__init__.py
+++ libem/core/model/__init__.py
@@ -2,7 +2,7 @@ from libem.core.model import (
     openai, llama
 )
 from libem.core import exec
-
+import libem
 
 def call(*args, **kwargs) -> dict:
     return exec.run_async_task(
@@ -14,6 +14,9 @@ async def async_call(*args, **kwargs) -> dict:
     if kwargs.get("model", "") == "llama3":
         return llama.call(*args, **kwargs)
     elif kwargs.get("model", "") == "llama3.1":
+        libem.calibrate({
+            "libem.match.prompt.output": "Do not provide explanation. Please only give your answer in the form of a single ‘yes’ or ‘no’.",
+        }, verbose=True)
         return llama.call(*args, **kwargs)
     else:
         return await openai.async_call(*args, **kwargs)
