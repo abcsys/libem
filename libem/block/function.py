fd0e6bd2af5c73eacb334b120d482c59ca58fdd7
David <davidspacewang@gmail.com>
2024-08-19 19:58:38 -0700

    feat: add option to partition by keys when blocking

--- libem/block/function.py
+++ libem/block/function.py
@@ -1,5 +1,8 @@
+import heapq
+import itertools
 from tqdm import tqdm
 from fuzzywuzzy import fuzz
+from operator import itemgetter
 from collections.abc import Iterable
 
 from libem.block import parameter
@@ -27,39 +30,92 @@ schema = {
 }
 
 
-def func(left, right=None):
+def func(left: Iterable[str | dict], 
+         right: Iterable[str | dict] | None = None, 
+         on: str | list | None = None) -> Iterable[dict]:
     if right is None:
-        return block(left)
+        return block(left, on)
     else:
-        return block_left_right(left, right)
+        return block_left_right(left, right, on)
 
 
-def block(records: Iterable[str | dict]) -> Iterable[dict]:
+def block(records: Iterable[str | dict], 
+          on: str | list | None = None) -> Iterable[dict]:
+    if on:
+        # group by
+        if isinstance(on, str):
+            records = sorted(records, key=itemgetter(on))
+            records = itertools.groupby(records, key=itemgetter(on))
+        elif isinstance(on, list):
+            records = sorted(records, key=itemgetter(*on))
+            records = itertools.groupby(records, key=itemgetter(*on))
+        
+        for _, group in tqdm(records, desc='Blocking'):
+            # duplicate the iterable to allow for nested iteration
+            left, right = itertools.tee(group, 2)
+            yield from block_similarity(left, right, block_same_index=False)
+    else:
+        # duplicate the iterable to allow for nested iteration
+        left, right = itertools.tee(records, 2)
+        yield from block_similarity(left, right, 
+                                    block_same_index=False, 
+                                    progress_bar=True)
+
+
+def block_left_right(left: Iterable[str | dict], 
+                     right: Iterable[str | dict],
+                     on: str | list | None = None) -> Iterable[dict]:
+    if on:
+        # group by
+        if isinstance(on, str):
+            left = sorted(left, key=itemgetter(on))
+            left = itertools.groupby(left, key=itemgetter(on))
+            right = sorted(right, key=itemgetter(on))
+            right = itertools.groupby(right, key=itemgetter(on))
+        elif isinstance(on, list):
+            left = sorted(left, key=itemgetter(*on))
+            left = itertools.groupby(left, key=itemgetter(*on))
+            right = sorted(right, key=itemgetter(*on))
+            right = itertools.groupby(right, key=itemgetter(*on))
+        
+        # inner join the two iterables
+        records = heapq.merge(left, right, key=lambda x: x[0])
+        last_key, last_val = None, []
+        for key, val in tqdm(records, desc='Blocking'):
+            if key == last_key:
+                last_key = None
+                yield from block_similarity(last_val, val)
+            else:
+                last_key = key
+                last_val = list(val)
+    else:
+        yield from block_similarity(left, right, progress_bar=True)
+
+
+def block_similarity(left: Iterable[str | dict], 
+                     right: Iterable[str | dict],
+                     block_same_index: bool = True,
+                     progress_bar: bool = False) -> Iterable[dict]:
+    ''' Block using string similarity. '''
+    
+    # set similarity and convert right iterable to list
     similarity = parameter.similarity()
+    right = list(right)
+    
+    if progress_bar:
+        left = tqdm(left, desc='Blocking')
 
-    for i, l in enumerate(
-            tqdm(records, desc='Blocking')):
+    for i, l in enumerate(left):
         left_str = convert_to_str(l)
 
         for j, r in enumerate(
-                tqdm(records, desc='Comparing', mininterval=0.01, leave=False)):
-            if i >= j:
+                tqdm(right, desc='Comparing', mininterval=0.01, leave=False)):
+            if not block_same_index and i >= j:
                 continue
+            
             right_str = convert_to_str(r)
             if fuzz.token_set_ratio(left_str, right_str) >= similarity:
                 yield {'left': l, 'right': r}
-
-
-def block_left_right(left: Iterable[str | dict], right: Iterable[str | dict]) -> Iterable[dict]:
-    similarity = parameter.similarity()
-
-    for l in tqdm(left, desc='Blocking'):
-        left_str = convert_to_str(l)
-        for r in tqdm(right, desc='Comparing', mininterval=0.01, leave=False):
-            right_str = convert_to_str(r)
-            if fuzz.token_set_ratio(left_str, right_str) >= similarity:
-                yield {'left': l, 'right': r}
-
 
 def convert_to_str(record: str | dict):
     match record:
