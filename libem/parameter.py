137c16b1bf2df8c7f4f8fc8664728edccd7293e0
Char15Xu <charlesxuty3@berkeley.edu>
2024-10-01 18:32:19 -0700

    syntax revision

--- libem/parameter.py
+++ libem/parameter.py
@@ -4,7 +4,7 @@ model = Parameter(
     default="gpt-4o-2024-08-06",
     options=["o1-preview", "o1-mini", "gpt-4o","gpt-4o-mini", "gpt-4",
              "gpt-4-turbo", "gpt-3.5-turbo",
-             "llama3", "llama3.1", "llama3.2", "claude-3-5-sonnet-20240620"]
+             "llama3", "llama3.1", "llama3.2-3b", "llama3.2-1b", "claude-3-5-sonnet-20240620"]
 )
 
 temperature = Parameter(
