4e63b91ae554069119dd845eaa35d6a0180584d2
David <davidspacewang@gmail.com>
2025-02-25 10:23:44 -0800

    add multimodal match example

--- examples/match.py
+++ examples/match.py
@@ -1,6 +1,14 @@
+import os
+import cv2
 import libem
 
 from libem.match.prompt import rules
+
+
+parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def get_image(name):
+    return cv2.imread(os.path.join(parent_directory, f"test/images/{name}.jpg"))
 
 
 def positive():
@@ -21,11 +29,27 @@ def negative():
     rules.add("Color differentiates entities.")
     is_match = libem.match(e1, e2)
 
+    print()
     print("Entity 1:", e1)
     print("Entity 2:", e2)
+    print("Match:", is_match['answer'])
+
+
+def multimodal():
+    e1 = {"text_fields": {"name": "fuji apple", "color": "red"},
+          "image_fields": [get_image("apple")]}
+    e2 = {"text_fields": {"name": "sweet seedless orange", "color": "orange"},
+          "image_fields": [get_image("orange1"), get_image("orange2")]}
+
+    is_match = libem.match(e1, e2)
+
+    print()
+    print("Entity 1:", e1["text_fields"], f"with {len(e1['image_fields'])} image(s)")
+    print("Entity 2:", e2["text_fields"], f"with {len(e2['image_fields'])} image(s)")
     print("Match:", is_match['answer'])
 
 
 if __name__ == '__main__':
     positive()
     negative()
+    multimodal()
